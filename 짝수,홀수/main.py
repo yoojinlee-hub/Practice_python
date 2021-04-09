4cec1bbb41937421a378eb10a7de41bfa6be46de
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2021-04-09 15:24:04 +0900

    Add files via upload

---
+++ 짝수,홀수/main.py
@@ -0,0 +1,10 @@
+def is_evenly_divisible(number):
+    # 코드를 작성하세요
+    return (number % 2 == 0)
+
+# 테스트
+print(is_evenly_divisible(3))
+print(is_evenly_divisible(7))
+print(is_evenly_divisible(8))
+print(is_evenly_divisible(218))
+print(is_evenly_divisible(317))
