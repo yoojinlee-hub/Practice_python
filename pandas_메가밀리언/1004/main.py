a04d1f39dd23fda4b88407d0859bd4cdd2cdc033
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2021-07-12 12:06:14 +0900

    Add files via upload

---
+++ pandas_메가밀리언/1004/main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# 코드를 작성하세요.
+df=pd.read_csv('data/mega_millions.csv',index_col=0)
+# 정답 출력
+df
---
+++ pandas_흔한이름/1003/main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# 코드를 작성하세요.
+df=pd.read_csv('data/popular_baby_names.csv')
+# 정답 출력
+df
