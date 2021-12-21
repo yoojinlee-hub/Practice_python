0be051c8cbfb2f68a16343a184f0ef329dcdfdf4
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2021-12-21 10:28:54 +0900

    Add files via upload

---
+++ 파일쓰기_단어장만들기/main.py
@@ -0,0 +1,11 @@
+with ('vocabulary.txt','w') as voca:
+    while True:
+        eng = input('영어 단어를 입력하세요: ')
+        if eng == 'q':
+            break
+        
+        kor = input('한국어 뜻을 입력하세요: ')
+        if kor == 'q':
+            break
+        
+        voca.write('{}: {}\n'.format(eng,kor))
