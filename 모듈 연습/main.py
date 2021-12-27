7a9dc0c470beb22f7aede4fe1a9305496f27f1e1
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2021-12-27 17:52:03 +0900

    Add files via upload

---
+++ 모듈 연습/main.py
@@ -0,0 +1,27 @@
+# cil 모듈을 임포트해 주세요
+import cil
+# cil 모듈의 display 함수를 직접 임포트해 주세요
+from cil import display
+
+img1 = cil.read_image('img1')
+img2 = cil.read_image('img2')
+
+inverted_img1 = cil.invert(img1)
+inverted_img2 = cil.invert(img2)
+
+print('원본 이미지')
+print('\nimage1:')
+display(img1)
+print('\nimage2:')
+display(img2)
+
+print('\n색상 반전된 이미지')
+print('\nimage1:')
+display(inverted_img1)
+print('\nimage2:')
+display(inverted_img2)
+
+# 채점 코드
+print()
+print('cil' in dir())
+print('display' in dir())
